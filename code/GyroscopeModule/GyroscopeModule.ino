bc5fe7839999082e0b2620d666efa42f3f4c0d02
Ivan Hawkes <ivan.hawkes@gmail.com>
2021-08-17 00:09:08 +1000

    A few formatting changes.

--- code/GyroscopeModule/GyroscopeModule.ino
+++ code/GyroscopeModule/GyroscopeModule.ino
@@ -1,103 +1,115 @@
 #include<Wire.h>
 
 // I2C address of the MPU-6050.
-const int MPU_addr = 0x68;
+const int mpuAddress = 0x68;
 
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+int16_t accelX;
+int16_t accelY;
+int16_t accelZ;
+int16_t temperature;
+int16_t gyroscopeX;
+int16_t gyroscopeY;
+int16_t gyroscopeZ;
+
 char line[132];
-
 
 
 void setup()
 {
-  Wire.begin();
-  Wire.beginTransmission(MPU_addr);
-  
-  // PWR_MGMT_1 register.
-  Wire.write(0x6B);
-  
-  // set to zero (wakes up the MPU-6050).
-  Wire.write(0);
-  
-  Wire.endTransmission(true);
+	Wire.begin();
+	Wire.beginTransmission(mpuAddress);
 
+	// PWR_MGMT_1 register.
+	Wire.write(0x6B);
 
-  Serial.begin(115200);
+	// set to zero (wakes up the MPU-6050).
+	Wire.write(0);
 
-  // Waiting for Serial Monitor.
-  while (!Serial);
+	Wire.endTransmission(true);
 
-  // Position the cursor.
-  sprintf(line, "%c[1;1H", 0x1b);
-  Serial.print(line);
-  
-  // Clear the line.
-  sprintf(line, "%c[2K", 0x1b);
-  Serial.print(line);
+	Serial.begin(115200);
 
-  Serial.println("AcX     AcY     AcZ     Temp    GyX     GyY     GyZ");
+	// Waiting for Serial Monitor.
+	while (!Serial);
+
+	// Position the cursor.
+	sprintf(line, "%c[1;1H", 0x1b);
+	Serial.print(line);
+
+	// Clear the line.
+	sprintf(line, "%c[2K", 0x1b);
+	Serial.print(line);
+
+	Serial.println("AcX     AcY     AcZ     Temp    GyX     GyY     GyZ");
 }
 
 
 void loop()
 {
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  
-  // Request a total of 14 registers.
-  Wire.requestFrom(MPU_addr, 14, true);
-  
-  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
-  AcX=Wire.read() << 8 | Wire.read();
-  
-  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  AcY=Wire.read() << 8 | Wire.read();
-  
-  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  AcZ=Wire.read() << 8 | Wire.read();
-  
-  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  Tmp=Wire.read() << 8 | Wire.read();
-  
-  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  GyX=Wire.read() << 8 | Wire.read();
-  
-  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  GyY=Wire.read() << 8 | Wire.read();
-  
-  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-  GyZ=Wire.read() << 8 | Wire.read();
-  
-  // Position the cursor.
-  sprintf(line, "%c[2;1H", 0x1b);
-  Serial.print(line);
-  
-  // Clear the line.
-  sprintf(line, "%c[2K", 0x1b);
-  Serial.print(line);
+	Wire.beginTransmission(mpuAddress);
+	
+	// Starting with register 0x3B (ACCEL_XOUT_H).
+	Wire.write(0x3B);
 
-  sprintf(line, "%c[36m", 0x1b);
-  Serial.print(line);
+	Wire.endTransmission(false);
 
-  // Note: I had issues with the value outputs when I used an sprintf for the output, thus this mess.
-  Serial.print(AcX);Serial.print("\t");
-  Serial.print(AcY);Serial.print("\t");
-  Serial.print(AcZ);Serial.print("\t");
-  
-  sprintf(line, "%c[31m", 0x1b);
-  Serial.print(line);
+	// Request a total of 14 registers.
+	Wire.requestFrom(mpuAddress, 14, true);
 
-  // Equation for temperature in degrees C from datasheet.
-  Serial.print(Tmp / 340.00 + 36.53);Serial.print("\t");
+	// 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+	accelX = Wire.read() << 8 | Wire.read();
 
-  sprintf(line, "%c[34m", 0x1b);
-  Serial.print(line);
+	// 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+	accelY = Wire.read() << 8 | Wire.read();
 
-  Serial.print(GyX);Serial.print("\t");
-  Serial.print(GyY);Serial.print("\t");
-  Serial.print(GyZ);Serial.print("\t");
-  Serial.println();
+	// 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+	accelZ = Wire.read() << 8 | Wire.read();
 
-  delay(250);
+	// 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+	temperature = Wire.read() << 8 | Wire.read();
+
+	// 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+	gyroscopeX = Wire.read() << 8 | Wire.read();
+
+	// 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+	gyroscopeY = Wire.read() << 8 | Wire.read();
+
+	// 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+	gyroscopeZ = Wire.read() << 8 | Wire.read();
+
+	// Position the cursor.
+	sprintf(line, "%c[2;1H", 0x1b);
+	Serial.print(line);
+
+	// Clear the line.
+	sprintf(line, "%c[2K", 0x1b);
+	Serial.print(line);
+
+	// Colour change.
+	sprintf(line, "%c[36m", 0x1b);
+	Serial.print(line);
+
+	// Note: I had issues with the value outputs when I used an sprintf for the output, thus this mess.
+	Serial.print(accelX); Serial.print("\t");
+	Serial.print(accelY); Serial.print("\t");
+	Serial.print(accelZ); Serial.print("\t");
+
+	// Colour change.
+	sprintf(line, "%c[31m", 0x1b);
+	Serial.print(line);
+
+	// Equation for temperature in degrees C from datasheet.
+	Serial.print(temperature / 340.00 + 36.53); Serial.print("\t");
+
+	// Colour change.
+	sprintf(line, "%c[34m", 0x1b);
+	Serial.print(line);
+
+	Serial.print(gyroscopeX); Serial.print("\t");
+	Serial.print(gyroscopeY); Serial.print("\t");
+	Serial.print(gyroscopeZ); Serial.print("\t");
+	
+	Serial.println();
+
+	delay(250);
 }
--- code/ScanI2C/ScanI2C.ino
+++ code/ScanI2C/ScanI2C.ino
@@ -3,53 +3,55 @@
 
 void setup()
 {
-  Wire.begin();
-  Serial.begin(115200);
-  
-  // Waiting for Serial Monitor.
-  while (!Serial);
+	Wire.begin();
+	Serial.begin(115200);
 
-  Serial.println("\nI2C Scanner");
+	// Waiting for Serial Monitor.
+	while (!Serial);
+
+	Serial.println("\nI2C Device Enumerator");
 }
 
 
 void loop()
 {
-  byte address;
-  int nDevices {0};
+	int nDevices {0};
 
-  Serial.println("Scanning...");
+	Serial.println("Scanning...");
 
-  for (address = 1; address < 127; address++ )
-  {
-	byte error;
+	for (byte address = 1; address < 127; address++ )
+	{
+		byte error {0};
 
-    // The i2c_scanner uses the return value of the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
+		// The i2c_scanner uses the return value of the Write.endTransmisstion to see if
+		// a device did acknowledge to the address.
+		Wire.beginTransmission(address);
+		error = Wire.endTransmission();
 
-    if (error == 0)
-    {
-      Serial.print("I2C device found at address 0x");
-      if (address < 16)
-        Serial.print("0");
-      Serial.print(address, HEX);
-      Serial.println("  !");
-      nDevices++;
-    }
-    else if (error == 4)
-    {
-      Serial.print("Unknown error at address 0x");
-      if (address < 16)
-        Serial.print("0");
-      Serial.println(address, HEX);
-    }
-  }
-  if (nDevices == 0)
-    Serial.println("No I2C devices found\n");
-  else
-    Serial.println("done\n");
+		if (error == 0)
+		{
+			Serial.print("I2C device found at address 0x");
+			if (address < 16)
+				Serial.print("0");
 
-  delay(4000);
+			Serial.print(address, HEX);
+			Serial.println("  !");
+			nDevices++;
+		}
+		else if (error == 4)
+		{
+			Serial.print("Unknown error at address 0x");
+			if (address < 16)
+				Serial.print("0");
+			
+			Serial.println(address, HEX);
+		}
+	}
+	
+	if (nDevices == 0)
+		Serial.println("No I2C devices found\n");
+	else
+		Serial.println("done\n");
+
+	delay(4000);
 }
